229161b8de7eba0be9884cf1609609d9db4679b7
yanisben <yanis.benneouala@gmail.com>
2022-09-08 17:06:18 +0200

    "commit"

--- lbc-DE-yanis/SQLQueries.sql
+++ lbc-DE-yanis/SQLQueries.sql
@@ -3,11 +3,11 @@
 create table if not exists transactions (date date, order_id int, client_id int, prop_id int, prod_price float, prod_qty int);
 create table if not exists product_nomenclature (product_id int, product_type text, product_name text);
 
-insert into transactions values ('2020/01/02', 1234, 999, 490756, 50, 1);
-insert into transactions values ('2020/01/03', 1234, 999, 389728, 3.56, 4);
-insert into transactions values ('2020/01/02', 3456, 845, 490756, 50, 2);
-insert into transactions values ('2020/01/03', 3456, 845, 549380, 300, 1);
-insert into transactions values ('2020/01/02', 3456, 845, 490756, 10, 6);
+insert into transactions values ('2019/01/02', 1234, 999, 490756, 50, 1);
+insert into transactions values ('2019/01/03', 1234, 999, 389728, 3.56, 4);
+insert into transactions values ('2019/01/02', 3456, 845, 490756, 50, 2);
+insert into transactions values ('2019/01/03', 3456, 845, 549380, 300, 1);
+insert into transactions values ('2019/01/02', 3456, 845, 490756, 10, 6);
 
 insert into product_nomenclature values (490756, 'MEUBLE', 'Chaise');
 insert into product_nomenclature values (389728, 'DECO', 'Boule de Noël');
