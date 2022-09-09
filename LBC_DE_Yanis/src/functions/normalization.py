ddf2e5f8da0c90406d000cc1d3c5090ff515f4c5
yanisben <yanis.benneouala@gmail.com>
2022-09-09 14:39:26 +0200

    "LBC_DE_TEST"

---
+++ LBC_DE_Yanis/src/functions/normalization.py
@@ -0,0 +1,12 @@
+from utils.configs import *
+from functions.ingestion import *
+from functions.merging import *
+import pandas as pd
+
+
+#  Normalization of dates
+
+def normalize_dates(df):
+    df['date'] = pd.to_datetime(df['date'])
+    df = df.astype(dtype='string')
+    return df
--- lbc-DE-yanis/src/functions/df_to_json.py
+++
@@ -1,9 +0,0 @@
-from utils.configs import *
-
-
-# Outputs to a json file
-
-
-def df_to_json(df):
-    df.to_json(JSON_RESULT_PATH, orient='records', indent=3, index=True, force_ascii=False)
-    print(f'Data pipeline executed, check output here : {JSON_RESULT_PATH}')
